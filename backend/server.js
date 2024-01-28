6b8909dea38a77b3c0758781dd7ac47c6ae49387
Ali <100287885+AlisahibAmiraslan@users.noreply.github.com>
2024-01-28 14:00:36 +0300

    update: cors middleware

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,23 @@ const CategoryRouter = require("./routes/category");
 
 // middleware
 app.use(express.json());
+
+// permison specific domain
+// var whitelist = ["http://localhost:3000"];
+
+// cors option. Nobody will see or get your api datas if search url on browser
+// var corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+// };
+
+// cors midleware
+// app.use(cors(corsOptions));
 app.use(cors());
 
 const port = process.env.PORT || 5000;
@@ -17,6 +34,12 @@ app.use("/api/auth", AuthRouter);
 app.use("/api/blogs", BlogRouter);
 app.use("/api/comments", CommentRouter);
 app.use("/api/categories", CategoryRouter);
+
+// not permision spesific endpoints
+// app.use("/api/auth",cors(corsOptions), AuthRouter);
+// app.use("/api/blogs",cors(corsOptions), BlogRouter);
+// app.use("/api/comments",cors(corsOptions), CommentRouter);
+// app.use("/api/categories",cors(corsOptions), CategoryRouter);
 
 const start = async () => {
   try {
